a7022319050573c132c8d388c1ef2a73229b07d7
pasha104 <pashakovall2012@gmail.com>
2026-08-19 10:47:00 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'busphoto-static-v3';
+const ASSETS = [
+  './', './index.html', './models.html', './interactive.html',
+  './busphoto-common.css?v=20260819-3', './busphoto-interactive.css?v=20260819-3',
+  './busphoto-common.js?v=20260819-3', './busphoto-interactive.js?v=20260819-3'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  const url = new URL(req.url);
+  if (url.origin !== self.location.origin) return;
+  event.respondWith(caches.match(req).then(cached => cached || fetch(req).then(res => {
+    const copy = res.clone();
+    caches.open(CACHE).then(cache => cache.put(req, copy)).catch(() => {});
+    return res;
+  }).catch(() => caches.match('./index.html'))));
+});
